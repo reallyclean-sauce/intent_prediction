fcd13e0069afe09bbb2e57f90db03fea85b397a0
reallyclean-sauce <jerico.arciaga@eee.upd.edu.ph>
2020-02-25 17:34:40 +0800

    Fixed the overall comprehensibility of the github codes.

---
+++ code/dataset.py
@@ -0,0 +1,65 @@
+from __future__ import print_function, division
+"""
+Contains a dataset class for
+validating the accuracy of the models
+in the Intent Prediction Architecture
+"""
+
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# Inherit features of torch Dataset
+class HeadPositionDataset(Dataset):
+    """
+    Middle of the Eyes Position Dataset
+    To be specific
+    """
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): the file path of the csv dataset
+            root_dir (string): Directory with all the images
+            transform (callable, optional): Optional transform
+                This can be resizing the image into 224x224
+        """
+        self.head_data = pd.read_csv('../dsp_intent_analyzer_dataset/head_data.csv')
+        self.root_dir = root_dir
+        self.transform = transform
+
+    # Need to be created;
+    def __len__(self):
+        return len(self.head_data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        file_idx = 0 # Contains the filename of the image
+        attr_idx = 5 # Contains the head position data
+        img_name = os.path.join(self.root_dir, self.head_data.iloc[idx, file_idx])
+        image = io.imread(img_name)
+
+        # This is not converted into float yet
+        head_pos = json.loads(self.head_data.iloc[idx, attr_idx])
+        head_pos = [head_pos['cx'],head_pos['cy']]
+        head_pos = np.asarray(head_pos)
+        head_pos = head_pos.astype('float').reshape(-1,2)
+
+        sample = {
+            'image': image,
+            'head_pos': head_pos
+        }
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
